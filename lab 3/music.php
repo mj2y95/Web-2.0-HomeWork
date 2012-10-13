189be072ee1362f4ca3feba5214749b6c076cec6
rpplusplus <i@rpplusplus.me>
2012-10-13 21:32:35 +0800

    差不多算是完成了吧...懒得折腾size了。。。

--- lab 3/music.php
+++ lab 3/music.php
@@ -14,38 +14,83 @@
 			<h2>Search Through Your Playlists and Music</h2>
 		</div>
 		
+
+		<?php
+			if ($_GET["playlist"] != NULL)
+			{
+				$tmp = $_GET["playlist"];
+				$arr = file(urldecode("songs/$tmp"));
+				if ($_GET["shuffle"] == "on")
+				{
+					shuffle($arr);
+				}
+		?>
+
+		<div id = "listarea">
+			<ul id="musiclist">
+				<?php
+				foreach ($arr as $item) {	
+					if ($item[0] != "#")
+					{
+						$fz = "songs/$item";
+						$fz = substr($fz, 0, strlen($fz)-2);
+				?>
+				<li class="mp3item">
+					<a href= <?= "\"songs/$item\"" ?> > <?= $item ?></a>
+					(<?= filesize($fz)?> b)
+				</li>
+				<?php
+					}
+				}
+				?>
+			</ul>
+		</div>
+
+		<?php
+			}
+			else
+			{
+		?>
+
 		
 		<div id="listarea">
 			<ul id="musiclist">
-				<li class="mp3item">
-					<a href="songs/Be More.mp3">Be More.mp3</a>
-					(5438375 b)
-				</li>
+				<?php
+					$files = glob("songs/*.mp3");
+					if ($_GET["shuffle"] == "on")
+					{
+						shuffle($files);
+					}
+					foreach ($files as $items) {
+						$arr = explode('/', $items);
+				?>
 
 				<li class="mp3item">
-					<a href="songs/Drift Away.mp3">Drift Away.mp3</a>
-					(5724612 b)
+					<a href= <?= "\"$items\"" ?> > <?= $arr[1] ?></a>
+
+					(<?= filesize($items) ?> b)
+					
 				</li>
 
-				<li class="mp3item">
-					<a href="songs/Hello.mp3">Hello.mp3</a>
-
-					(1871110 b)
-				</li>
-
-				<li class="mp3item">
-					<a href="songs/Panda Sneeze.mp3">Panda Sneeze.mp3</a>
-					(58 b)
-				</li>
-
+				<?php
+					}
+					$files = glob("songs/*.m3u");
+					foreach ($files as $items) {
+						$arr = explode('/', $items);
+						$tmp = urlencode($arr[1]);
+				?>
 				<li class="playlistitem">
-					<a href="music.php?playlist=mypicks.txt">mypicks.txt</a>
+					<a href=<?= "music.php?playlist=$tmp" ?> > <?= $arr[1]?></a>
 				</li>
 
-				<li class="playlistitem">
-					<a href="music.php?playlist=playlist.txt">playlist.txt</a>
-				</li>
+				<?php
+					}
+				?>
 			</ul>
 		</div>
+		<?php
+			}
+		?>
 	</body>
 </html>
+
